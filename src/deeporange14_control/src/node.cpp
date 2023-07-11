8a2509923761df7d7c6ba4783e32df6edea6f2b9
Priyanshu Rawat <83747696+priyanshurawat1509@users.noreply.github.com>
2023-07-11 15:14:01 -0400

    Update node.cpp

--- src/deeporange14_control/src/node.cpp
+++ src/deeporange14_control/src/node.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 
   // create StateMachine object
   
-  deeporange14::RosHealthMonitor n_rosHealthmonitor(nh,priv_nh);
+
   deeporange14::StateMachine n_statemachine(nh, priv_nh);
 
   // create Data Logger object
